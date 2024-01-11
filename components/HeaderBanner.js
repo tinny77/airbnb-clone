4b18c51c34fb9f14d53731db7180d51cceccbc9b
tinny77 <tinny77@gmail.com>
2024-01-11 15:06:16 +0100

    Layout: Header Banner

---
+++ components/HeaderBanner.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+function HeaderBanner() {
+	return (
+		<div className="relative h-[300px] sm:h-[400px] lg:h-[500px]">
+			<Image
+				src="/banner.webp"
+				fill
+				style={{ objectFit: 'cover', objectPosition: 'bottom' }}
+			/>
+			<div className="absolute grid grid-col w-full h-full place-content-center text-center">
+				<p className="text-sm md:text-base">Not sure where to go? Perfect.</p>
+				<button className="inline-block mx-auto text-purple-500 bg-white px-6 py-2 lg:py-3  mt-2 shadow-md rounded-full font-semibold hover:font-bold hover:shadow-xl active:scale-110 hover:scale-110 transition duration-150 cursor-pointer">
+					I'm flexible
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default HeaderBanner;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Header from '@/components/Header';
+import HeaderBanner from '@/components/HeaderBanner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
+			<HeaderBanner />
 		</div>
 	);
 }
