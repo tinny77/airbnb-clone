7ddf5bed9303365a52ef9b9ccaf1f3d86adb2f88
tinny77 <tinny77@gmail.com>
2024-01-11 17:26:35 +0100

    Fixed unescaped character

--- components/HeaderBanner.js
+++ components/HeaderBanner.js
@@ -11,7 +11,7 @@ function HeaderBanner() {
 			<div className="absolute grid grid-col w-full h-full place-content-center text-center">
 				<p className="text-sm md:text-base">Not sure where to go? Perfect.</p>
 				<button className="inline-block mx-auto text-purple-500 bg-white px-6 py-2 lg:py-3  mt-2 shadow-md rounded-full font-semibold hover:font-bold hover:shadow-xl active:scale-110 hover:scale-110 transition duration-150 cursor-pointer">
-					I'm flexible
+					I&apos;m flexible
 				</button>
 			</div>
 		</div>
